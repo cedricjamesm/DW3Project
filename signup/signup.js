49a653957d364960782ce72e7ab84bbff8698810
jordan artzy-mccendie <jartzy01@gmail.com>
2024-04-18 20:38:52 -0400

    fixed the redirection path

--- signup/signup.js
+++ signup/signup.js
@@ -57,7 +57,7 @@ function submitSignupForm(formData) {
     })
     .then(data => {
         if (data.success) {
-            window.location.href = 'index.html';  // Adjust the redirect path as needed
+            window.location.href = 'index.php';
         } else {
             document.getElementById('error-messages').innerHTML = "<p>" + data.message + "</p>";
         }
